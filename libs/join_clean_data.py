2f30cb5bb98d3f121e383b3936478f518647c8a7
josebonet <jose.bonet@irbbarcelona.org>
2021-08-31 17:11:33 +0200

    delete debuggers

--- libs/join_clean_data.py
+++ libs/join_clean_data.py
@@ -16,15 +16,14 @@ def main(folder_data, output):
     all_year_dfs = {} 
     for folder in [f.path for f in os.scandir(folder_data) if f.is_dir()]:
         df_year = pd.DataFrame(); year = folder.rsplit('/', 1)[-1]
-        import pdb;pdb.set_trace()
+
         for file in os.listdir(folder):
             wb = load_workbook(filename=os.path.join(folder, file))
             ws = wb.active
             df = pd.DataFrame(ws.values)
             df = df.rename(columns=df.iloc[0]).drop(axis=0, index=0)
             df_year = pd.concat([df_year, df])
-            # import pdb;pdb.set_trace()
-        import pdb;pdb.set_trace()
+
         all_year_dfs[year] = df_year
 
 
